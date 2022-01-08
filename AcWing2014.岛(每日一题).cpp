1f60bbd57db109df34fb109757634f880aeb0356
030666 <2028800763@qq.com>
2022-01-08 21:40:02 +0800

    更改

--- AcWing2014.岛(每日一题).cpp
+++ AcWing2014.岛(每日一题).cpp
@@ -23,16 +23,16 @@ int main ()
     h[n+1]=0;//后续代码可能会用到第n+1个位置，需要把第n+1个位置清空
     
     for(int i=1;i<=n;i++) q[i]={h[i],i};
-    sort(q+1,q+n+1);
+    sort(q+1,q+n+1);//将去重之后的柱子高度排序 
     int res=1,cnt=1;//cnt代表初始时小岛数量
     
     for(int i=1;i<=n;i++)
     {
-        int k=q[i].y;
+        int k=q[i].y;//取出来此时这个柱子的下标 
         if(h[k-1]<h[k]&&h[k+1]<h[k]) cnt--;//如果中间的柱子比两边都大
         else if(h[k-1]>h[k]&&h[k+1]>h[k]) cnt++;//如果中间的柱子比两边都小
         
-        if(q[i].x!=q[i+1].x)
+        if(q[i].x!=q[i+1].x)//防止出现两个相等高度的柱子没挨着导致没去重 
             res=max(res,cnt);
     }
     printf("%d\n",res);
