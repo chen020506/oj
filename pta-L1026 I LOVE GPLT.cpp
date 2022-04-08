386ccc68ed14962bc88a07c70dc571616da5c79f
030666 <2028800763@qq.com>
2022-04-08 22:07:56 +0800

    天梯赛

---
+++ pta-L1026 I LOVE GPLT.cpp
@@ -0,0 +1,11 @@
+// 时间：2022.05.06 21点43分
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str("I Love GPLT");
+    for(int i=0;i<str.size();i++)
+        cout<<str[i]<<endl;
+    return 0;
+}
