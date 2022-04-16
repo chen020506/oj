4aa5b55c099542a30d3678617de51659f431a684
030666 <2028800763@qq.com>
2022-04-16 22:30:06 +0800

    0416天梯赛练习

--- test.cpp
+++ test.cpp
@@ -1,68 +1,95 @@
 #include<iostream>
 using namespace std;
-class note
+struct note
 {
-public:
     int x;
     int y;
-    int f;
-    int s;
 };
-
+char a[20][21];
+int getnum(int i,int j)
+{
+    int sum,x,y;
+    sum=0;
+    x=i,y=j;
+    while(a[x][y]!='#')
+    {
+        if(a[x][y]=='G')
+            sum++;
+        x--;
+    }
+    x=i,y=j;
+    while(a[x][y]!='#')
+    {
+        if(a[x][y]=='G')
+            sum++;
+        x++;
+    }
+    x=i,y=j;
+    while(a[x][y]!='#')
+    {
+        if(a[x][y]=='G')
+            sum++;
+        y--;
+    }
+    x=i,y=j;
+    while(a[x][y]!='#')
+    {
+        if(a[x][y]=='G')
+            sum++;
+        y++;
+    }
+    return sum;
+}
 int main ()
 {
-    note que[2501];
-    int a[51][51]={0},book[51][51]={0};
+    struct note que[401];
+    int head,tail;
+    int book[20][20]={0};
+    int i,j,k,sum,mmax=0,mx,my,n,m,startx,starty,tx,ty;
     int next[4][2]={
         {0,1},
         {1,0},
         {0,-1},
         {-1,0}
     };
-    int head,tail;
-    int i,j,k,n,m,startx,starty,p,q,tx,ty,flag;
-    cin>>n>>m;
-    for(i=1;i<=n;i++)
-        for(j=1;j<=m;j++)
-            cin>>a[i][j];
-    cin>>startx>>starty>>p>>q;
+    cin>>n>>m>>startx>>starty;
+    for(i=0;i<n;i++)
+        cin>>a[i];
+    //队列初始化
     head=1,tail=1;
     que[tail].x=startx;
     que[tail].y=starty;
-    que[tail].f=0;
-    que[tail].s=0;
     tail++;
     book[startx][starty]=1;
-
-    flag=0;
+    mmax=getnum(startx,starty);
+    mx=startx;
+    my=starty;
     while(head<tail)
     {
-        for(k=0;k<=3;k++)
+        for(k=0;k<3;k++)
         {
             tx=que[head].x+next[k][0];
             ty=que[head].y+next[k][1];
 
-            if(tx<1 || tx>n || ty<1 || ty>m)
+            if(tx<0 || tx>n-1 || ty<0 || ty>m-1)
                 continue;
-            if(a[tx][ty]==0&&book[tx][ty]==0)
+            if(a[tx][ty]=="."&&book[tx][ty]==0)
             {
                 book[tx][ty]=1;
                 que[tail].x=tx;
                 que[tail].y=ty;
-                que[tail].f=head;
-                que[tail].s=que[head].s+1;
                 tail++;
-            }
-            if(tx==p&&ty==q)
-            {
-                flag==1;
-                break;
+                sum=getnum(tx,ty);
+                if(sum>mmax)
+                {
+                    mmax=sum;
+                    mx=tx;
+                    my=ty;
+                }
             }
         }
-        if(flag==1)
-            break;
         head++;
     }
-    printf("%d ",que[tail-1].s);
+    printf("%d %d =%d",mx,my,mmax);
     return 0;
 }
