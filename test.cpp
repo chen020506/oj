5efe2d78881a51e875b36574b35ab8e14fe02f68
030666 <2028800763@qq.com>
2022-04-20 22:35:08 +0800

    邻接表

--- test.cpp
+++ test.cpp
@@ -1,49 +1,19 @@
-#include<iostream>
-using namespace std;
-
-
-int main ()
+int n,m,i;
+int u[6],v[6],w[6];
+int first[5],next[5];
+cin>>n>>m;
+for(i=1;i<=n;i++)
+    first[i]=-1;
+for(i=1;i<=m;i++)
 {
-    int e[10][10],dis[10],book[10],i,j,n,m,t1,t2,t3,u,v,mmin;
-    int inf=99999999;
-    cin>>n>>m;
-    for(i=1;i<=n;i++)
-        for(j=1;j<=n;j++)
-            if(i==j) e[i][j]=0;
-            else e[i][j]=inf;
-    for(i=1;i<=m;i++)
-    {
-        cin>>t1>>t2>>t3;
-        e[t1][t2]=t3;
-    }
-    //初始化dis数组
-    for(i=1;i<=n;i++)
-        dis[i]=e[1][i];
-    for(i=1;i<=n;i++)
-        book[i]=0;
-    book[1]=1;
-    
-    for(i=1;i<=n-1;i++)
-    {
-        mmin=inf;
-        for(j=1;j<=n;j++)
-        {
-            if(book[j]==0&&dis[j]<mmin)
-            {
-                mmin=dis[j];
-                u=j;
-            }
-        }
-        book[u]=1;//u就是离1最近的一个点
-        for(v=1;v<=n;v++)
-        {
-            if(e[u][v]<inf)
-                dis[v]=min(dis[u]+e[u][v],dis[v]);
-        }
-    }
-    for(i=1;i<=n;i++)
-    {
-        printf("%d ",dis[i]);
-    }
-    return 0;
+    cin>>u[i]>>v[i]>>w[i];
+    next[i]=first[u[i]];
+    first[u[i]]=i;
+}
+//邻接表的遍历
+k=first[1];
+while(k!=-1)
+{
+    cout<<u[k]<<v[k]<<w[k]<<endl;
+    k=next[k];
 }
