d4bcd1bf9b4803d2342376ebf4cf4198da6f3a12
030666 <2028800763@qq.com>
2022-04-21 23:40:12 +0800

    邻接表

--- test.cpp
+++ test.cpp
@@ -1,19 +1,50 @@
-int n,m,i;
-int u[6],v[6],w[6];
-int first[5],next[5];
-cin>>n>>m;
-for(i=1;i<=n;i++)
-    first[i]=-1;
-for(i=1;i<=m;i++)
+const int N = 1010, M = 1010;
+
+int h[N], e[M], w[M], nxt[M], eidx;
+
+void add(int u, int v, int weight)   // 添加有向边 u->v, 权重为weight
 {
-    cin>>u[i]>>v[i]>>w[i];
-    next[i]=first[u[i]];
-    first[u[i]]=i;
+    e[eidx] = v;        // 记录边的终点
+    w[eidx] = weight;   // 记录边的权重
+    nxt[eidx] = h[u];   // 将下一条边指向结点u此时的第一条边
+    h[u] = eidx;        // 将结点u的第一条边的编号改为此时的eidx
+    eidx++;             // 递增边的编号edix, 为将来使用
 }
-//邻接表的遍历
-k=first[1];
-while(k!=-1)
+
+void iterate(int u)   // 遍历结点u的所有邻边
 {
-    cout<<u[k]<<v[k]<<w[k]<<endl;
-    k=next[k];
+    // 从u的第一条边开始遍历，直到eid==-1为止
+    for(int eid = h[u]; eid != -1; eid = nxt[eid])
+    {
+        int v = e[eid];
+        int weight = w[eid];
+        cout << u << "->" << v << ", weight: " << weight << endl;
+    }
 }
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    memset(h, -1, sizeof h);  // 初始化h数组为-1
+    eidx = 0;                 // 初始化边的编号为0
+
+    while(m--)
+    {
+        int u, v, weight;
+        cin >> u >> v >> weight;
+        add(u, v, weight);
+    }
+
+    for(int u = 1; u <= n; ++u)
+        iterate(u);
+
+    return 0;
+}
+
+
+作者：houghstc
+链接：https://www.acwing.com/blog//content/4663/
+来源：AcWing
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
