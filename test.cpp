6fda812c868a957611fcbb8cbbeca3641861f27c
030666 <2028800763@qq.com>
2022-04-22 23:35:13 +0800

    全排列

--- test.cpp
+++ test.cpp
@@ -1,50 +1,39 @@
-const int N = 1010, M = 1010;
-
-int h[N], e[M], w[M], nxt[M], eidx;
-
-void add(int u, int v, int weight)   // 添加有向边 u->v, 权重为weight
+//获得之前的字典序
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int a[3]={3,2,1};
+int main ()
 {
-    e[eidx] = v;        // 记录边的终点
-    w[eidx] = weight;   // 记录边的权重
-    nxt[eidx] = h[u];   // 将下一条边指向结点u此时的第一条边
-    h[u] = eidx;        // 将结点u的第一条边的编号改为此时的eidx
-    eidx++;             // 递增边的编号edix, 为将来使用
-}
-
-void iterate(int u)   // 遍历结点u的所有邻边
-{
-    // 从u的第一条边开始遍历，直到eid==-1为止
-    for(int eid = h[u]; eid != -1; eid = nxt[eid])
+    do
     {
-        int v = e[eid];
-        int weight = w[eid];
-        cout << u << "->" << v << ", weight: " << weight << endl;
-    }
+        cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<endl;
+    }while(prev_permutation(a,a+3));
 }
+  
+3 2 1
+3 1 2
+2 3 1
+2 1 3
+1 3 2
+1 2 3
 
-int main()
+//获得之后的字典序
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int a[3]={1,2,3};
+int main ()
 {
-    int n, m;
-    cin >> n >> m;
-
-    memset(h, -1, sizeof h);  // 初始化h数组为-1
-    eidx = 0;                 // 初始化边的编号为0
-
-    while(m--)
+    do
     {
-        int u, v, weight;
-        cin >> u >> v >> weight;
-        add(u, v, weight);
-    }
-
-    for(int u = 1; u <= n; ++u)
-        iterate(u);
-
-    return 0;
+        cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<endl;
+    }while(next_permutation(a,a+3));
 }
-
-
-作者：houghstc
-链接：https://www.acwing.com/blog//content/4663/
-来源：AcWing
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+  
+1 2 3
+1 3 2
+2 1 3
+2 3 1
+3 1 2
+3 2 1
