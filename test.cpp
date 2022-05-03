b01d520f2add5d1e096c0cd18d38b0a3ec52d632
030666 <2028800763@qq.com>
2022-05-04 00:15:33 +0800

    最大堆

--- test.cpp
+++ test.cpp
@@ -9,12 +9,12 @@ void siftdown(int i)
     int t,flag=0;
     while(i*2<=n&&flag==0)
     {
-        if(h[i]>h[i*2])
+        if(h[i]<h[i*2])
             t=i*2;
         else
             t=i;
         if(i*2+1<=n)
-            if(h[t]>h[i*2+1])
+            if(h[t]<h[i*2+1])
                 t=i*2+1;
         if(t!=i){
             swap(h[t],h[i]);
