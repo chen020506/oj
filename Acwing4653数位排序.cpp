a113e00575a9f2beb38a7c7aebcbefaea9d65484
chen0505 <2028800763@qq.com>
2023-01-05 21:20:03 +0800

    寒假每日一题

---
+++ Acwing4653数位排序.cpp
@@ -0,0 +1,31 @@
+//  时间：2023.01.05  21点17分 
+#include<iostream>
+#include<algorithm>
+using namespace std;
+const int N=1e6+10;
+struct node{
+    int x,y;
+}a[N];
+bool cmp(node a,node b)
+{
+    if(a.y!=b.y)return a.y<b.y;
+    return a.x<b.x;
+}
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    for(int i=1;i<=n;i++){
+        a[i].x=i;
+        int t=i;
+        while(t){
+            a[i].y+=t%10;
+            t/=10;
+        }
+    }
+    sort(a+1,a+1+n,cmp);
+    cout<<a[m].x;
+}       
+
+
+
